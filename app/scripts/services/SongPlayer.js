94b721b8ad4208e2861f8ee738a709844c2a886c
Heather Lucas <hlucas@gmail.com>
2017-02-23 20:17:10 -0500

    revise assignment7-services2

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -56,7 +56,7 @@
                 
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
