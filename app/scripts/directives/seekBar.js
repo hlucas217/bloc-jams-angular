ac699b23757689b36b93bd7d43c7799319d3de57
Heather Lucas <hlucas@gmail.com>
2017-02-28 23:28:09 -0500

    Finish assignment9-directives1

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -34,7 +34,7 @@
                  };
                  
                  scope.thumbStyle = function() {
-                    return {width: percentString()};      
+                    return {left: percentString()};      
                  };
                  
                  scope.onClickSeekBar = function(event) {
